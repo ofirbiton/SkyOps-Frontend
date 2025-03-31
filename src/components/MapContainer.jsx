b70e60092a402b5844a70393b8fcb876e6922a58
Noe-Mignolet <146870718+Noe-Mignolet@users.noreply.github.com>
2025-03-31 23:46:14 +0300

    Update MapContainer.jsx

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -6,7 +6,7 @@ export default function MapContainer() {
     // בעת טעינת המסמך, יוצרים את המפה בתוך האלמנט עם id="map"
     window.jQuery(document).ready(function () {
       window.govmap.createMap("map", {
-        token: "5a4b8472-b95b-4687-8179-0ccb621c7990",
+        token: "7f0764a7-7a42-4214-a98b-cb669559d9cb",
         layers: ["GASSTATIONS", "PARCEL_HOKS", "KSHTANN_ASSETS", "bus_stops", "PARCEL_ALL"],
         showXY: true,
         identifyOnClick: true,

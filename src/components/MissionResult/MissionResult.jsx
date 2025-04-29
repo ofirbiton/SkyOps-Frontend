eec3aebc11103948c50abd2dd1006b4344895609
Noe-Mignolet <146870718+Noe-Mignolet@users.noreply.github.com>
2025-04-29 11:49:21 +0300

    Update MissionResult.jsx

--- src/components/MissionResult/MissionResult.jsx
+++ src/components/MissionResult/MissionResult.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import Header from "./Header/Header.js";
+import Header from "./Header/Header";
 
 export default function MissionResult({ onToggleTaskMode }) {
   const { state } = useLocation();
